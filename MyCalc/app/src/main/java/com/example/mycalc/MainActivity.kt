1a9ee167eb427f80d73c95bedaf38007b596442b
JohnTitor <user@USER-no-MacBook-Air.local>
2020-06-06 21:47:53 +0900

    kotlin　アプリ（簡単なやつ）アップ

---
+++ MyCalc/app/src/main/java/com/example/mycalc/MainActivity.kt
@@ -0,0 +1,162 @@
+package com.example.mycalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.activity_main.view.num0
+
+class MainActivity : AppCompatActivity() {
+
+    var nStr : String = ""
+    val nList = ArrayList<Double>() // arraylist to store numbers
+    val oList = ArrayList<Char>() // arraylist to store operations
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        num0.setOnClickListener {
+            formula.text = "${formula.text}0"
+            nStr += "0"
+        }
+        num1.setOnClickListener {
+            formula.text = "${formula.text}1"
+            nStr += "1"
+        }
+        num2.setOnClickListener {
+            formula.text = "${formula.text}2"
+            nStr += "2"
+        }
+        num3.setOnClickListener {
+            formula.text = "${formula.text}3"
+            nStr += "3"
+        }
+        num4.setOnClickListener {
+            formula.text = "${formula.text}4"
+            nStr += "4"
+        }
+        num5.setOnClickListener {
+            formula.text = "${formula.text}5"
+            nStr += "5"
+        }
+        num6.setOnClickListener {
+            formula.text = "${formula.text}6"
+            nStr += "6"
+        }
+        num7.setOnClickListener {
+            formula.text = "${formula.text}7"
+            nStr += "7"
+        }
+        num8.setOnClickListener {
+            formula.text = "${formula.text}8"
+            nStr += "8"
+        }
+        num9.setOnClickListener {
+            formula.text = "${formula.text}9"
+            nStr += "9"
+        }
+        dot.setOnClickListener {
+            formula.text = "${formula.text}."
+            nStr += "."
+        }
+        equal.setOnClickListener {
+            formula.text = "${formula.text}="
+            addList(nStr)
+            var result = calcualte().toString()
+            formula.text = result
+            nStr = result
+            nList.clear()
+            oList.clear()
+        }
+        prass.setOnClickListener {
+            formula.text = "${formula.text}+"
+            addList(nStr,'+')
+            nStr = ""
+        }
+        minus.setOnClickListener {
+            formula.text = "${formula.text}-"
+            addList(nStr,'-')
+            nStr = ""
+        }
+        cross.setOnClickListener {
+            formula.text = "${formula.text}*"
+            addList(nStr,'*')
+            nStr = ""
+        }
+        div.setOnClickListener {
+            formula.text = "${formula.text}/"
+            addList(nStr,'/')
+            nStr = ""
+        }
+        delete.setOnClickListener {
+            var formulaStr = formula.text.toString()
+            if (!formulaStr.isEmpty()) {
+                formula.text = formulaStr.subSequence(0,formulaStr.lastIndex)
+            }
+            if (!nStr.isEmpty()) {
+                nStr = nStr.substring(0, nStr.lastIndex)
+            }
+        }
+        percent.setOnClickListener {
+            formula.text = "${formula.text}%"
+        }
+        prass_minus.setOnClickListener {
+
+        }
+        Clear.setOnClickListener {
+            formula.text = ""
+            nStr = ""
+            nList.clear()
+            oList.clear()
+        }
+
+    }
+
+    fun addList(str : String, ope : Char) {
+        try {
+            var num = str.toDouble()
+            nList.add(num)
+            oList.add(ope)
+        }catch(e:Exception){
+            formula.text = "Numeric error"
+        }
+    }
+
+    fun addList(str : String) {
+        try {
+            var num = str.toDouble()
+            nList.add(num)
+        }catch(e:Exception){
+            formula.text = "Numeric error"
+        }
+    }
+    fun calcualte() : Double {
+
+        var i = 0
+        while (i < oList.size) {
+            //do multiplication and division first
+            if(oList.get(i) == '*' || oList.get(i) == '/') {
+                var result = if (oList.get(i) == '*') nList.get(i) * nList.get(i+1) else nList.get(i) / nList.get(i+1)
+                nList.set(i,result)
+                nList.removeAt(i+1)
+                oList.removeAt(i)
+                i--
+            }
+            // change subtraction to addition
+            else if(oList.get(i) == '-'){
+                oList.set(i,'+')
+                nList.set(i+1,nList.get(i+1) * -1)
+            }
+            i++
+        }
+
+        // get sum
+        var result = 0.0
+        for (i in nList){
+            result += i
+        }
+
+        return result
+    }// end fun calcualte
+}
